5384390acf035a84a7a9cbaee524b87208b9561d
Vishal Y.V <VishalYella@gmail.com>
2023-06-05 11:15:09 +0530

    Finished Project

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
   <>
     <div className='flex justify-center items-center h-screen w-full Tablet:overflow-y-auto Tablet:overflow-x-hidden Tablet:h-auto'>
-      <div className='flex justfiy-center items-center h-auto w-auto m-5 flex-row bg-Gray Tablet:flex-col Tablet:w-96'>
+      <div className='flex justfiy-center items-center h-auto w-auto m-5 flex-row Tablet:flex-col Tablet:w-96'>
         <Sedans />
         <SUVs />
         <Luxury />
